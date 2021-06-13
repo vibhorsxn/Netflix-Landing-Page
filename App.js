836385e0a846f4fecc2930502f56d053a3f17d07
Vibhor <vibhorsxn@gmail.com>
2021-06-13 13:41:36 +0530

    CSS is added & all the comp. is placed

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import requests from "./requests";
+import Row from "./Row";
+import Banner from "./Banner.js";
+import Nav from "./Nav.js";
+
+function App() {
+  return (
+    <div className="app" >
+      <Nav />
+      <Banner />
+      <Row title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow />
+      <Row title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+      />
+      <Row title="Top Rated"
+        fetchUrl={requests.fetchTopRated}
+      />
+      <Row title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+      />
+    </div>
+  );
+}
+
+export default App;
