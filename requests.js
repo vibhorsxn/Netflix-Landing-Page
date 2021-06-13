641d9d0925746597a367d7be0ea60f268c7ad440
Vibhor <vibhorsxn@gmail.com>
2021-06-13 13:33:35 +0530

    Api key is added for the request

---
+++ requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "6e10f5b6f8c2a52a6cee8accac406e9c";
+
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default requests;
